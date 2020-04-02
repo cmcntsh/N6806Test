2fdc67d361213420b5c32f45d68f8871cab33cbf
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-01 20:00:36 -0600

    javascript navigation

---
+++ JavaScript/navigation.js
@@ -0,0 +1,4 @@
+document.write('        <p class = "top-line">Pages</p>');
+document.write('        <ul class = "nav-list">');
+document.write('            <li><a href="index.html">Page 1</a></li>');
+document.write('        </ul>');
