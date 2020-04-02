ece6463c8a1f80758c0d1eb0fa9795444467e916
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-01 20:06:12 -0600

    Add page 2 and nav

--- JavaScript/navigation.js
+++ JavaScript/navigation.js
@@ -1,4 +1,5 @@
 document.write('        <p class = "top-line">Pages</p>');
 document.write('        <ul class = "nav-list">');
 document.write('            <li><a href="index.html">Page 1</a></li>');
+document.write('            <li><a href="page2.html">Page 1</a></li>');
 document.write('        </ul>');
