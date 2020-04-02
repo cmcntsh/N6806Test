d2728ae91ebcbe8d164510f87862585b9a9a8158
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-02 08:20:19 -0600

    Add GitHub to VSCode page

--- JavaScript/navigation.js
+++ JavaScript/navigation.js
@@ -1,5 +1,6 @@
 document.write('        <p class = "top-line">Pages</p>');
 document.write('        <ul class = "nav-list">');
-document.write('            <li><a href="index.html">Page 1</a></li>');
-document.write('            <li><a href="page2.html">Page 2</a></li>');
+document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/">Home</a></li>');
+document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/Pages/page2.html">Page 2</a></li>');
+document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/Pages/AddGitHubRepoToVSCode_2020-04-02.html">Add GitHub Repo To VSCode</a></li>');
 document.write('        </ul>');
