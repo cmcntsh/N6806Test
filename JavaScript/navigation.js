fe7c991ff28677050f01088b8eac6ecd57436385
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-04 11:40:32 -0600

    add page 3

--- JavaScript/navigation.js
+++ JavaScript/navigation.js
@@ -2,5 +2,6 @@ document.write('        <p class = "top-line">Pages</p>');
 document.write('        <ul class = "nav-list">');
 document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/">Home</a></li>');
 document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/Pages/page2.html">Page 2</a></li>');
+document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/Pages/page3.html">Page 3</a></li>');
 document.write('            <li><a href="https://cmcntsh.github.io/N6806Test/Pages/AddGitHubRepoToVSCode_2020-04-02.html">Add GitHub Repo To VSCode</a></li>');
 document.write('        </ul>');
